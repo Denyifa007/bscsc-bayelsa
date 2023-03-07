7bc9c25aa3227464eb44a64cae190ca1615bf774
denyifa007 <denyifasamuel@hotmail.com>
2023-03-07 09:27:24 +0100

    commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,10 +2,11 @@ import React from "react";
 import { MDBRow, MDBCol } from "mdb-react-ui-kit";
 import diri from "../Assets/WhatsApp Image 2023-03-02 at 13.54.42.jpeg";
 import "../Style/Body.css";
+import { Link } from "react-router-dom";
 
 const Body = () => {
   return (
-    <MDBRow className="container">
+    <MDBRow className="container-fluid">
       <MDBCol id="abu" md="8">
         <h1>ABOUT US</h1>
         <h5>
@@ -17,7 +18,7 @@ const Body = () => {
           Volunteers and the Bayelsa Vigilante into an umbrella body. To fight
           crime in the state and the need to spread policing to the hinterlands.
         </h5>
-        <button id="btn-about">About us</button>
+        <Link to='Aboutus'><button id="btn-about">About us</button></Link>
       </MDBCol>
       <MDBCol id="image" md="4">
         <img src={diri} alt="" id="diri" className="img-fluid"  />
--- src/Components/Head.js
+++ src/Components/Head.js
@@ -5,20 +5,22 @@ import corplogo from "../Assets/bylogo (1).png";
 
 const Head = () => {
   return (
-    <div>
+    <div className="Header">
       <div className="bg-image">
         <img src={logo3} className="logo3" alt="Sample"  />
         <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
-          <div className="d-flex justify-content-center align-items-center h-100">
-            <h1 id="bscsc" className="text-white mb-2">
-              Bayelsa State Community Safety Corp
-            </h1>
-            <p id="community">
-              “The community corps is designed to support, partner and 
-              collaborate with the <br /> statutory federal security agencies in
-              efforts to rid the state of crime and criminal <br /> elements.”
-            </p>
-            <img src={corplogo} alt="" id="corplogo" />
+          <div className="head">
+            <div className="Header-contents container-fluid">
+              <h1 className="Header_text text-white mb-2 mt-3 animate__animated animate__heartBeat   animation-duration: 2s;">
+                Bayelsa State Community Safety Corp
+              </h1>
+              <p className="community animate__animated animate__backInLeft">
+                “The community corps is designed to support, partner and 
+                collaborate with the <br /> statutory federal security agencies in
+                efforts to rid the state of crime and criminal <br /> elements.”
+              </p>
+            </div>
+            <img src={corplogo} alt="" id="corplogo"/>
           </div>
         </div>
       </div>
